10a91a85425c704e6854e710721ecbfd6fc83040
John Willard <jgwil2@gmail.com>
2017-01-28 23:30:41 -0500

    take command line args for msg and n, print msg n times

--- app/Main.hs
+++ app/Main.hs
@@ -3,18 +3,20 @@ module Main where
 import System.IO (readFile)
 import Data.Time (getCurrentTime)
 import Data.Aeson (encode)
+import System.Environment (getArgs)
+import Control.Monad (replicateM_)
 
 import Lib
 
 main :: IO ()
 main = do
-    putStrLn (greet "Johnny")
-    putStrLn (greet "World")
-    printNumbers
-    -- printConfig
-    print (encode numbers)
+    args <- getArgs
+    let message = head args
+    let n = read (last args) :: Int
+    replicateM_ n (print message)
 
 numbers = [1,2,3,4] :: [Int]
+numbersJson = encode numbers
 
 greet :: String -> String
 greet name = "Hello, " ++ name ++ "!"
