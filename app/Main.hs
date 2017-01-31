473d353c3bac743c0382356a21901e4f11729c5e
John Willard <jgwil2@gmail.com>
2017-01-30 22:31:15 -0500

    playing with scotty

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
+import GHC.Generics
 import System.IO (readFile)
-import Data.Time (getCurrentTime)
-import Data.Aeson (encode)
 import System.Environment (getArgs)
+import Data.Monoid ((<>))
 import Control.Monad (replicateM_)
+import Data.Time (getCurrentTime)
+import Data.Aeson (encode, FromJSON, ToJSON)
+import Web.Scotty
 
 import Lib
 
 main :: IO ()
 main = do
+    putStrLn "Starting server"
+    scotty 3000 $ do
+        get "/hello/:name" $ do
+            name <- param "name"
+            text ("Hello, " <> name <> "!")
+        get "/users" $ do
+            json allUsers
+        get "/users/:id" $ do
+            id <- param "id"
+            json $ head $ filter (matchesId id) allUsers
+
+data User = User { userId :: Int, userName :: String } deriving (Show, Generic)
+instance ToJSON User
+instance FromJSON User
+
+bob = User { userId = 1, userName = "bob" }
+jen = User { userId = 2, userName = "jen" }
+
+allUsers = [bob, jen]
+
+matchesId id user = userId user == id
+
+cat :: IO ()
+cat = do
     file <- fmap head getArgs >>= readFile
     putStrLn file
 
